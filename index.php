c4cf64d7d6ba907703187c7f5c55b5e05c5ad93f
Paweł Zawadzki <pawel.zawadzki2@teb.edu.pl>
2025-03-12 14:52:39 +0100

    Add Smarty template engine integration and update index.php to use templates

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 require_once 'vendor/autoload.php';
 
 use Steampixel\Route;
+use Smarty\Smarty;
+
+$smarty = new Smarty();
+$smarty->setTemplateDir('templates');
+$smarty->setCompileDir('templates_c');
+$smarty->setCacheDir('cache');
+$smarty->setConfigDir('configs');
 
 Route::add('/', function() {
-    echo 'Welcome to the homepage!';
+    global $smarty;
+    $smarty->display('index.tpl');
 });
 
 Route::run('/tebChan');
